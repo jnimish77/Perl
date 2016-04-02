52b869913560876956aee93e713ef64b96d0b37c
Nimish Joshi <jnimish77@users.noreply.github.com>
2016-04-01 20:56:26 -0500

    Create Second_value_from_each_array_element.pl

---
+++ Second_value_from_each_array_element.pl
@@ -0,0 +1,40 @@
+
+    #!/usr/bin/perl
+    use strict;
+    use warnings;
+
+    my@array = ("aditya","john","pasha","linkon","obama","angel","nelson","Anderson","jasmine","nilda");
+
+
+my$string0=$array[0];
+my$string1=$array[1];
+my$string2=$array[2];
+my$string3=$array[3];
+my$string4=$array[4];
+my$string5=$array[5];
+my$string6=$array[6];
+my$string7=$array[7];
+my$string8=$array[8];
+my$string9=$array[9];
+
+my$char0=substr($string0,1,1);
+
+print "$char0";
+my$char1=substr($string1,1,1);
+print "$char1";
+my$char2=substr($string2,1,1);
+print "$char2";
+my$char3=substr($string3,1,1);
+print "$char3";
+my$char4=substr($string4,1,1);
+print "$char4";
+my$char5=substr($string5,1,1);
+print "$char5";
+my$char6=substr($string6,1,1);
+print "$char6";
+my$char7=substr($string7,1,1);
+print "$char7";
+my$char8=substr($string8,1,1);
+print "$char8";
+my$char9=substr($string9,1,1);
+print "$char9\n";
